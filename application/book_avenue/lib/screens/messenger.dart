199366e480c79c03d7afc20141d9a636443c1114
ta1231 <54363784+ta1231@users.noreply.github.com>
2020-08-18 14:34:49 +0900

    messengerUi
    uiuimessengerui

--- application/book_avenue/lib/screens/messenger.dart
+++ application/book_avenue/lib/screens/messenger.dart
@@ -1,8 +1,60 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:bookavenue/screens/chatui/recent_chats.dart';
+import 'package:bookavenue/screens/chatui/favorite_contacts.dart';
+import 'package:bookavenue/screens/chatui/category_selector.dart';
+
 
 class messenger extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
-  }
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          iconSize: 30.0,
+          color: Colors.white,
+          onPressed: () {},
+        ),
+        title: Text(
+          'Chats',
+          style: TextStyle(
+            fontSize: 28.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          CategorySelector(),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).accentColor,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: Column(
+                children: <Widget>[
+                  FavoriteContacts(),
+                  RecentChats(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );  }
 }
